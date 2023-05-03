919fbcc39366bb584a724c55ed20e29ab9236f83
enzom-uy <enzom.dev@gmail.com>
2023-05-03 17:14:41 -0300

    Refactor typescript types

---
+++ src/types/commands.ts
@@ -0,0 +1,20 @@
+import { ChatInputCommandInteraction } from 'discord.js'
+
+export type Data = {
+  options: []
+  name: string
+  name_localizations: undefined
+  description: string
+  description_localizations: string | undefined
+  default_permission: any
+  default_member_permissions: any
+  dm_permission: any
+  nsfw: any
+}
+
+export type Execute = (i: ChatInputCommandInteraction) => any
+
+export type Command = {
+  data: Data
+  execute: Execute
+}
