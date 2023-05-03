5dc25f09106b56bb9319d24c4000fb424f5510f1
enzom-uy <enzom.dev@gmail.com>
2023-05-03 17:19:47 -0300

    Delete ping test command

--- src/commands/ping/ping.ts
+++
@@ -1,7 +0,0 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
-
-export const data = new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong.')
-
-export const execute = async (interaction: ChatInputCommandInteraction): Promise<void> => {
-  await interaction.reply('Pongaso!')
-}
